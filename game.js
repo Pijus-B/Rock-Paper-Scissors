f5c585a32cd8189ba99e29334bd295b289cea2a2
Pijus-B <p.bogusi@gmail.com>
2024-09-26 21:58:55 +0300

    playround function

--- game.js
+++ game.js
@@ -13,15 +13,24 @@ function getComputerChoice(x){
      choice = choice.toLowerCase();
      if (choice === "rock" || choice === "paper" || choice === "scissors")
      return choice;
-     else return "Invalid choice, try again";
+     else alert("Invalid choice, try again");
  }
  let humanScore = 0;
  let computerScore = 0;
 
  function playRound(humanScore, computerScore){
+    if (getHumanChoice() === "paper" && getComputerChoice() === "rock" ||
+        getHumanChoice() === "rock" &&  getComputerChoice() === "scissors" ||
+        getHumanChoice() === "scissors" && getComputerChoice() === "paper"){
+            console.log("You Won!");
+            humanScore++;
+        }
+    else if (getHumanChoice() === "paper" && getComputerChoice() === "scissors" ||
+             getHumanChoice() === "rock" && getComputerChoice() === "paper" ||
+             getHumanChoice() === "scissors" && getComputerChoice() === "rock"){
+                console.log("You Lost");
+                computerScore++;
+             }
+    else console.log("It's Tied!"); 
 
  }
- const humanSelection = getHumanChoice();
- const computerSelection = getComputerChoice();
-
- playround(humanSelection, computerSelection);
